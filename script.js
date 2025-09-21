2f3ab82a6773cc53941d7534e06fa05ed09a188e
Faheem <faheemxxx456@gmail.com>
2025-09-21 15:29:55 +0530

    added play and pause function

--- script.js
+++ script.js
@@ -7,79 +7,76 @@ const shuffleButton = document.getElementById("shuffle");
 
 const allSongs = [
   {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+  },
+  {
     id: 1,
-    title: "Main Title",
-    artist: "Ramin Djawadi",
-    duration: "1:46",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
   },
   {
     id: 2,
-    title: "The Rains of Castamere",
-    artist: "The National",
-    duration: "2:23",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
   },
   {
     id: 3,
-    title: "Light of the Seven",
-    artist: "Ramin Djawadi",
-    duration: "6:02",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"
+    title: "Cruising for a Musing",
+    artist: "Quincy Larson",
+    duration: "3:34",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cruising-for-a-musing.mp3",
   },
   {
     id: 4,
-    title: "Winterfell",
-    artist: "Ramin Djawadi",
-    duration: "2:25",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3"
+    title: "Never Not Favored",
+    artist: "Quincy Larson",
+    duration: "3:35",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/never-not-favored.mp3",
   },
   {
     id: 5,
-    title: "The Night King",
-    artist: "Ramin Djawadi",
-    duration: "3:38",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3"
+    title: "From the Ground Up",
+    artist: "Quincy Larson",
+    duration: "3:12",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/from-the-ground-up.mp3",
   },
   {
     id: 6,
-    title: "Dracarys",
-    artist: "Ramin Djawadi",
-    duration: "2:47",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3"
+    title: "Walking on Air",
+    artist: "Quincy Larson",
+    duration: "3:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/walking-on-air.mp3",
   },
   {
     id: 7,
-    title: "The Winds of Winter",
-    artist: "Ramin Djawadi",
-    duration: "2:20",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3"
+    title: "Can't Stop Me. Can't Even Slow Me Down.",
+    artist: "Quincy Larson",
+    duration: "3:52",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/cant-stop-me-cant-even-slow-me-down.mp3",
   },
   {
     id: 8,
-    title: "The Bear and the Maiden Fair",
-    artist: "The Hold Steady",
-    duration: "2:56",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"
+    title: "The Surest Way Out is Through",
+    artist: "Quincy Larson",
+    duration: "3:10",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/the-surest-way-out-is-through.mp3",
   },
   {
     id: 9,
-    title: "Power Is Power",
-    artist: "SZA, The Weeknd, Travis Scott",
-    duration: "3:23",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+    title: "Chasing That Feeling",
+    artist: "Quincy Larson",
+    duration: "2:43",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/chasing-that-feeling.mp3",
   },
-  {
-    id: 10,
-    title: "Jenny of Oldstones",
-    artist: "Florence + The Machine",
-    duration: "3:00",
-    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3"
-  }
 ];
-
-
-
 
 const audio = new Audio();
 let userData = {
